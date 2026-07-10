6824483afa876c4f20cb2af284c6f30b070e967a
abintsam001-sys <abintsam001@gmail.com>
2026-07-10 12:26:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function renderStatsBar() {
   if (!el || typeof SCRIPTS === 'undefined') return;
 
   const totalDownloads = 9;
-  const totalUsers = 191;
+  const totalUsers = 203;
 
   const items = [
     {
